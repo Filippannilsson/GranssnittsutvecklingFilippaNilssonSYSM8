fd87227da3570a3e3e86301b4df93e7d0b8ca797
Filippannilsson <filippanilsson95@gmail.com>
2025-05-27 14:42:41 +0200

    Småfix med layout

--- src/components/MenuFilter.jsx
+++ src/components/MenuFilter.jsx
@@ -19,77 +19,70 @@ function MenuFilter({
 
       <div className="first-row">
         <div className="filter-bar">
-          <button
-            className={
-              selectedCategory === "all" ? "all-btn active" : "all-btn"
-            }
-            onClick={() => handleCategoryClick("all")}
-          >
-            All
-          </button>
-          <button
-            className={
-              selectedCategory === "starters"
-                ? "starters-btn active"
-                : "starters-btn"
-            }
-            onClick={() => handleCategoryClick("starters")}
-          >
-            Starters
-          </button>
-          <button
-            className={
-              selectedCategory === "dips" ? "dips-btn active" : "dips-btn"
-            }
-            onClick={() => handleCategoryClick("dips")}
-          >
-            Dips
-          </button>
-          <button
-            className={
-              selectedCategory === "mains" ? "mains-btn active" : "mains-btn"
-            }
-            onClick={() => handleCategoryClick("mains")}
-          >
-            Mains
-          </button>
-          <button
-            className={
-              selectedCategory === "desserts"
-                ? "desserts-btn active"
-                : "desserts-btn"
-            }
-            onClick={() => handleCategoryClick("desserts")}
-          >
-            Desserts
-          </button>
-          <button
-            className={
-              selectedCategory === "drinks" ? "drinks-btn active" : "drinks-btn"
-            }
-            onClick={() => handleCategoryClick("drinks")}
-          >
-            Drinks
-          </button>
-        </div>
-
-        <div className="right-controls">
-          {/* <div className="sort-container">
-            <select id="sort" name="sort" defaultValue="">
-              <option value="" disabled>
-                Sort By
-              </option>
-              <option value="price">Price</option>
-              <option value="name">Name</option>
-            </select>
-          </div> */}
-
-          <button
-            className="clear-filter-btn"
-            onClick={() => handleCategoryClick("all")}
-          >
-            Clear X
-          </button>
+          <div className="filter-bar-btns">
+            <button
+              className={
+                selectedCategory === "all" ? "all-btn active" : "all-btn"
+              }
+              onClick={() => handleCategoryClick("all")}
+            >
+              All
+            </button>
+            <button
+              className={
+                selectedCategory === "starters"
+                  ? "starters-btn active"
+                  : "starters-btn"
+              }
+              onClick={() => handleCategoryClick("starters")}
+            >
+              Starters
+            </button>
+            <button
+              className={
+                selectedCategory === "dips" ? "dips-btn active" : "dips-btn"
+              }
+              onClick={() => handleCategoryClick("dips")}
+            >
+              Dips
+            </button>
+            <button
+              className={
+                selectedCategory === "mains" ? "mains-btn active" : "mains-btn"
+              }
+              onClick={() => handleCategoryClick("mains")}
+            >
+              Mains
+            </button>
+            <button
+              className={
+                selectedCategory === "desserts"
+                  ? "desserts-btn active"
+                  : "desserts-btn"
+              }
+              onClick={() => handleCategoryClick("desserts")}
+            >
+              Desserts
+            </button>
+            <button
+              className={
+                selectedCategory === "drinks"
+                  ? "drinks-btn active"
+                  : "drinks-btn"
+              }
+              onClick={() => handleCategoryClick("drinks")}
+            >
+              Drinks
+            </button>
+          </div>
+          <div className="clear-section">
+            <button
+              className="clear-filter-btn"
+              onClick={() => handleCategoryClick("all")}
+            >
+              Clear filter X
+            </button>
+          </div>
         </div>
       </div>
 
